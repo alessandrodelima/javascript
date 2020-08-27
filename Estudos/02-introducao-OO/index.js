c48067de40ed463049779ddfda81ce6e6eac39b2
Alessandro Lima <alessandro.lima@msn.com>
2020-08-27 13:31:46 -0300

    Trilha Alura  JavaScript - Orientação a Objetos

---
+++ Estudos/02-introducao-OO/index.js
@@ -0,0 +1,30 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    rg;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Alessandro";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.rg = 4588765
+cliente1.Saldo = 0;
+
+cliente2.nome = "Isabella";
+cliente2.cpf = 88877766605;
+cliente2.agencia = 1001;
+cliente2.Saldo = 0;
+
+
+
+
+
+console.log(cliente1, cliente2);
+
+
+
