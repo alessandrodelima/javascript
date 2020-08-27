70cfbf7597d520b762c79a380a6d252dcbf7724b
Alessandro Lima <alessandro.lima@msn.com>
2020-08-27 17:02:43 -0300

    Tipo de valor e tipo de referência

--- Estudos/02-introducao-OO/index.js
+++ Estudos/02-introducao-OO/index.js
@@ -1,30 +1,29 @@
-class Cliente {
-    nome;
-    cpf;
-    agencia;
-    rg;
-    saldo;
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Alessandro";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.rg = 4588765
-cliente1.Saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Isabella";
 cliente2.cpf = 88877766605;
-cliente2.agencia = 1001;
-cliente2.Saldo = 0;
 
+const contaCorrenteAlessandro = new ContaCorrente();
+contaCorrenteAlessandro.agencia = 1001;
+contaCorrenteAlessandro.cliente = cliente1;
+contaCorrenteAlessandro.depositar(500);
 
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
+let valor = 200;
+contaCorrenteAlessandro.transferir(valor, conta2);
 
+console.log("Valor: ", valor);
+console.log(conta2);
 
-console.log(cliente1, cliente2);
 
 
 
