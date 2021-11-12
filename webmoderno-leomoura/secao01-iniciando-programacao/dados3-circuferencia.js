4a8cdd54954cde199ad4fe4b70455bfda859e82b
Alessandro Lima <alessandro.lima@anater.org>
2021-11-12 16:17:44 -0300

    algoritmo circuferencia.

---
+++ webmoderno-leomoura/secao01-iniciando-programacao/dados3-circuferencia.js
@@ -0,0 +1,11 @@
+// PI  3.141592
+// PI * raio * raio 
+
+const PI = 3.141592;
+const raio = 10;
+const areaDoCirculo = PI * raio * raio;
+
+console.log("O valor da área é: "+areaDoCirculo+ "m2.");
+console.log(Math.PI);
+
+
