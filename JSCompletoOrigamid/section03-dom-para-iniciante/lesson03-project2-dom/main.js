fb04e40b82f40a3f188ff79085d658df0d77e7ab
Alessandro Lima <alessandro.lima@anater.org>
2022-01-21 16:50:22 -0300

    Usando Js no projeto

---
+++ JSCompletoOrigamid/section03-dom-para-iniciante/lesson03-project2-dom/main.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+console.log(gridSection.length);
+console.log(gridSection[0]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkinterno = document.querySelector('[href^="#"]');
+console.log(linkinterno);
+console.log(linkinterno.href);
