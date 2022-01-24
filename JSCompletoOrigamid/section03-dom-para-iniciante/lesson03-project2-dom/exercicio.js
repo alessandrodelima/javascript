a2cb0b1a985f6aa9a8aef073771f67d42ade9fb2
Alessandro Lima <alessandro.lima@anater.org>
2022-01-24 17:45:21 -0300

    Usando Js no projeto

---
+++ JSCompletoOrigamid/section03-dom-para-iniciante/lesson03-project2-dom/exercicio.js
@@ -0,0 +1,13 @@
+//Exercícios 
+// Retorne no console todas as imagens do site
+const img = document.querySelectorAll('img');
+console.log('Exercício - retornando as imagens do site',img);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
+
--- JSCompletoOrigamid/section03-dom-para-iniciante/lesson03-project2-dom/main.js
+++ JSCompletoOrigamid/section03-dom-para-iniciante/lesson03-project2-dom/main.js
@@ -15,3 +15,29 @@ console.log(primeiraUl);
 const linkinterno = document.querySelector('[href^="#"]');
 console.log(linkinterno);
 console.log(linkinterno.href);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+console.log(animaisImg[1]);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
+
+console.log(gridSectionHTML[0]);
+console.log(gridSectionNode[0]);
+
+primeiraUl.classList.add('grid-section');
+
+gridSectionNode.forEach(function(item, index) {
+    console.log(index);
+});
+
+
+const arrayGrid = Array.from(gridSectionHTML);
+
+arrayGrid.forEach(function(item) {
+    console.log(item);
+})
