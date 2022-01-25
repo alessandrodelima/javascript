00fcb695458d5b49f243e510dd105c9428d99d7a
Alessandro Lima <alessandro.lima@anater.org>
2022-01-25 17:52:26 -0300

    Usando Js no projeto

--- JSCompletoOrigamid/section03-dom-para-iniciante/lesson03-project2-dom/exercicio.js
+++ JSCompletoOrigamid/section03-dom-para-iniciante/lesson03-project2-dom/exercicio.js
@@ -4,10 +4,20 @@ const img = document.querySelectorAll('img');
 console.log('Exercício - retornando as imagens do site',img);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+console.log("Exercício - retornando imagens que comecem com a palavra imagem", imagensAnimais );
 
 // Selecione todos os links internos (onde o href começa com #)
+const linksInterno = document.querySelectorAll('[href^="#"]');
+//const linksInterno = document.querySelectorAll('[href^="#animais"]');
+console.log("Exercício - selecionar os links internos onde começa com #", linksInterno);
 
 // Selecione o primeiro h2 dentro de .animais-descricao
+const h2Animais = document.querySelector('.animais-descricao h2');
+console.log("Exercício ", h2Animais);
 
 // Selecione o último p do site
-
+const paragrafos = document.querySelectorAll('p');
+//console.log(paragrafos);
+console.log("Exercício - último parágrafo", paragrafos[paragrafos.length - 1]); // ou 
+console.log("Exercício - último parágrafo", paragrafos[--paragrafos.length]); 
